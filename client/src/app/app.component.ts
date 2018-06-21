52a378d6a6f1c6a4f0b24b2a5ca5b1536838fc9e
damienbod <damien_bod@hotmail.com>
2018-06-21 20:23:58 +0200

    adding login, logout button

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  isAuthorizedSubscription: Subscription | undefined;
+  isAuthorized = false;
+
+  constructor(public oidcSecurityService: OidcSecurityService) {
+    if (this.oidcSecurityService.moduleSetup) {
+      this.doCallbackLogicIfRequired();
+    } else {
+      this.oidcSecurityService.onModuleSetup.subscribe(() => {
+        this.doCallbackLogicIfRequired();
+      });
+    }
+  }
+
+  ngOnInit() {
+    this.isAuthorizedSubscription = this.oidcSecurityService.getIsAuthorized().subscribe(
+      (isAuthorized: boolean) => {
+        this.isAuthorized = isAuthorized;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.isAuthorizedSubscription) {
+      this.isAuthorizedSubscription.unsubscribe();
+    }
+    this.oidcSecurityService.onModuleSetup.unsubscribe();
+  }
+
+  private doCallbackLogicIfRequired() {
+    if (window.location.hash) {
+      console.log('doing stuff');
+      this.oidcSecurityService.authorizedCallback();
+    }
+  }
 }
