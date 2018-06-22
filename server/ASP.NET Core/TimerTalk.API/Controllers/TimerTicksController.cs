c9c8960345129e9309176ffcee1d4c31bdb0b3d7
damienbod <damien_bod@hotmail.com>
2018-06-22 08:25:10 +0200

    code clean up

--- server/ASP.NET Core/TimerTalk.API/Controllers/TimerTicksController.cs
+++ server/ASP.NET Core/TimerTalk.API/Controllers/TimerTicksController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using AutoMapper;
-using System.Collections.Generic;
 using System.Linq;
 using TimerTalk.API.Repositories;
 using TimerTalk.API.Models;
